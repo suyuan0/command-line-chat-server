c5edd84c05eae8a5f2a27ca614dd8b090f6edb78
wanwan <oonm78027@gmail.com>
2025-09-26 19:24:57 +0800

    feat(database): 将TypeORM与MySQL和环境配置集成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,42 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+interface DBInterface {
+  DB_HOST: string;
+  DB_PORT: number;
+  DB_USER: string;
+  DB_PASSWD: string;
+  DB_DATABASE: string;
+  NODE_ENV: string;
+}
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<DBInterface>) => {
+        return {
+          type: 'mysql',
+          entities: [],
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USER'),
+          password: configService.get('DB_PASSWD'),
+          database: configService.get('DB_DATABASE'),
+          timezone: '+08:00',
+          synchronize: configService.get('NODE_ENV') === 'development',
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
