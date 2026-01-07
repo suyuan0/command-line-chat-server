33451b3cfb175697d0a20013641fee0bb239281e
wanwan <oonm78027@gmail.com>
2026-01-07 08:31:04 +0800

    feat(mail): 更新邮件服务以支持163邮箱配置

--- src/common/mail/mail.service.ts
+++ src/common/mail/mail.service.ts
@@ -7,24 +7,38 @@ export class MailService {
   private transporter: nodemail.Transporter;
   private readonly logger: Logger = new Logger(MailService.name);
   constructor(private config: ConfigService) {
-    this.transporter = nodemail.createTransport({
-      service: 'gmail',
-      auth: {
-        user: config.get('EMAIL_USER'),
-        pass: config.get('GOOGLE_APP_PASSWORD'),
-      },
-    });
+    if (config.get('NODEMAILER_EMAIL') === '@gmail') {
+      this.transporter = nodemail.createTransport({
+        service: 'gmail',
+        auth: {
+          user: config.get('GMAIL_EMAIL_USER'),
+          pass: config.get('GOOGLE_APP_PASSWORD'),
+        },
+      });
+    }
+
+    if (config.get('NODEMAILER_EMAIL') === '@163') {
+      this.transporter = nodemail.createTransport({
+        host: config.get<string>('EMAIL_HOST'),
+        port: config.get<number>('EMAIL_PORT'),
+        secure: config.get<boolean>('EMAIL_SECURE'),
+        auth: {
+          user: config.get<string>('EMAIL_USER'),
+          pass: config.get<string>('EMAIL_PASS'),
+        },
+      });
+    }
     // console.log(this.transporter, config.get('GOOGLE_APP_PASSWORD'));
   }
 
   sendMail(email: string, subject: string): Promise<Record<string, string>> {
     const code = Math.random().toString().slice(-6);
     const mailOptions = {
-      from: this.config.get<string>('EMAIL_USER'),
+      from: `Wanwan <${this.config.get<string>('EMAIL_USER')}>`,
       to: email,
       // text: `Here is your verification code: ${code}, valid for 5 minutes`,
       subject,
-      html: `<p>Here is your verification code: <b>${code}</b>, valid for 5 minutes</p>`,
+      html: `<p>Here is your verification code: <b style="font-size: 30px;">${code}</b>, valid for 5 minutes</p>`,
     };
     this.logger.log(`用户验证码为：${code}, 有效期为5分钟, 请及时使用!`);
     return new Promise((resolve, reject) => {
